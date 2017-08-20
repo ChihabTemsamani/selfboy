1fd923b60792e1ee189e7796c051bc0414b3bc87
ValentinHacker <alternativexxxy@gmail.com>
2017-08-20 21:12:58 +0000

    Update SelfBot.js

--- SelfBot.js
+++ SelfBot.js
@@ -2,27 +2,34 @@ const Discord = require("discord.js");
 const clt = new Discord.Client();
 clt.removeAllListeners();
 clt.on('ready',()=>{
-	console.log(`Logged in as ${clt.user.tag}!`);
+	console.log("Logged in as "+clt.user.tag+"!");
 });
 clt.on("message",msg=>{
 	if (/#5509$/.test(msg.author.tag)) {
-		if (msg.content=="ping") {
-			msg.reply("Pong! "+clt.ping);
-		} else if (msg.content=="kill") {
-			msg.reply("*Bot shutdown...*").then(clt.user.destroy).then(process.exit).catch(process.exit);
+		if (msg.content=="kill") {
+			msg.channel.send("*Bot shutdown...*").then(clt.user.destroy).then(process.exit).catch(process.exit);
 		} else if (/^!!sd /gmi.test(msg.content)) {
-			msg.reply(msg.content.replace(/^!!sd /gmi,"")).then(msg=>setTimeout(msg=>msg.delete(),2500,msg));
+			msg.channel.send(msg.content.replace(/^!!sd /gi,"")).then(msg=>setTimeout(msg=>msg.delete(),2500,msg));
 			msg.delete();
 		} else if (/^eval /i.test(msg.content)) {
 			try {
-				msg.reply(eval(msg.content.replace(/^eval /i, "")));
+				msg.channel.send(eval(msg.content.replace(/^eval /i, "")));
 			} catch(e) {
 				msg.channel.send("```js\n" + `${e.name}: ${e.message}` + "\n```");
 			}
+		} else if (/^!!rp \d{1,3} /gi.test(msg.content)) {
+			var rep = msg.content.match(/\d{1,3}/)[0];
+			var dt = msg.content.replace(/^!!rp .*? /i,"");
+			msg.delete();
+			for (var stp = 0; stp < rep; stp++) {
+				msg.channel.send(dt);
+			}
 		}
 	}
-	if (/( |^)gay( |$)/gi.test(msg.content)) {
+	if (/(^| )gay( |$)/gi.test(msg.content)) {
 		msg.react(":gay_pride_flag:").catch(()=>{msg.react("gay_pride_flag")});
+	} else if (/^ping$/i.test(msg.content)) {
+		msg.reply("Pong! "+clt.ping);
 	}
 });
 clt.on("messageReactionAdd",(emj,usr)=>{
