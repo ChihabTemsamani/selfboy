4d34bb643be3dea10a97bd83fa21814e091a9109
ValentinHacker <alternativexxxy@gmail.com>
2017-08-23 18:07:57 +0300

    Update SelfBot.js

--- SelfBot.js
+++ SelfBot.js
@@ -42,33 +42,32 @@ clt.on("message",msg=>{
 				msg.delete();
 			}
 		}
-	}
-	if (/(^| |\W)gay($| |\W)/gi.test(msg.content)&&!/((u|o)w(u|o))|New Game/gi.test(msg.guild.name)&&!(/#5509$/.test(msg.author.tag)&&msg.content.includes("```"))) {
-        	msg.react("🏳️‍🌈");
-	}
-	if (/(\W| |^)ountv?v?c?k?($| |\W)/gi.test(msg.content)) {
-		msg.delete();
-		return;
-	}
-	if (/^!!ping$/i.test(msg.content)) {
-        	msg.reply("Pong! "+clt.ping);
-	} else if (/^!!rg .+?/i.test(msg.content)) {
-        	msg.reply(msg.content.replace(/^!!rg /i,"").split("").map(val=>{var vl=val.toLowerCase();if("abcdefghijklmnopqrstuvwxyz".indexOf(vl)>=0){return ":regional_indicator_"+vl+":";}else if(/\d/.test(val)){return ":"+["zero","one","two","three","four","five","six","seven","eight","nine"][Number(val)]+":"}else if(vl={"?":"question","!":"exclamation","*":"asterisk","#":"hash","-":"heavy_minus_sign","+":"heavy_plus_sign","/":"heavy_division_sign","$":"heavy_doller_sign"}[val]){return ":"+vl+":";}else{return val;}}).join(""));
-		msg.delete();
-	} else if (/^!!he?lp/i.test(msg.content)) {
-		msg.reply("```\n!!ping\n!!rg text --> converts your speech to emojis\n!!hlp\n!!sd text --> sends message and deletes after 0.25 seconds\n!!rp number text --> repeats text 'number' times.\nbot automatically reacts with :gay_pride_flag: when message contains the word 'gay' and upvotes reactions...\nDM @ValentinHacker#5509 for disable...```");
-	} else if (/^!!sd /gmi.test(msg.content)) {
-           	msg.reply(msg.content.replace(/^!!sd /i,"")).then(msg=>setTimeout(msg=>msg.delete(),2500,msg));
-        	msg.delete();
-        } else if (/^!!rp \d{1,2} /i.test(msg.content)) {
-        	let rep = Number(msg.content.split(" ")[1]||1), dt = msg.content.split(" ")[2]||"null";
-        	msg.delete();
-        	for (var stp = 0; stp < rep; stp++) {
-                	msg.reply(dt);
-        	}
-        } else if (/^!!id$/i.test(msg.content)) {
-		msg.reply("<\\@"+msg.author.id+">");
-	}
+		if (/(^| |\W)gay($| |\W)/gi.test(msg.content)&&!/((u|o)w(u|o))|New Game/gi.test(msg.guild.name)&&!(/#5509$/.test(msg.author.tag)&&msg.content.includes("```"))) {
+        		msg.react("🏳️‍🌈");
+		}
+		if (/(\W| |^)ountv?v?c?k?($| |\W)/gi.test(msg.content)) {
+			msg.delete();
+			return;
+		}
+		if (/^!!ping$/i.test(msg.content)) {
+        		msg.reply("Pong! "+clt.ping);
+		} else if (/^!!rg .+?/i.test(msg.content)) {
+        		msg.reply(msg.content.replace(/^!!rg /i,"").split("").map(val=>{var vl=val.toLowerCase();if("abcdefghijklmnopqrstuvwxyz".indexOf(vl)>=0){return ":regional_indicator_"+vl+":";}else if(/\d/.test(val)){return ":"+["zero","one","two","three","four","five","six","seven","eight","nine"][Number(val)]+":"}else if(vl={"?":"question","!":"exclamation","*":"asterisk","#":"hash","-":"heavy_minus_sign","+":"heavy_plus_sign","/":"heavy_division_sign","$":"heavy_doller_sign"}[val]){return ":"+vl+":";}else{return val;}}).join(""));
+			msg.delete();
+		} else if (/^!!he?lp/i.test(msg.content)) {
+			msg.reply("```\n!!ping\n!!rg text --> converts your speech to emojis\n!!hlp\n!!sd text --> sends message and deletes after 0.25 seconds\n!!rp number text --> repeats text 'number' times.\nbot automatically reacts with :gay_pride_flag: when message contains the word 'gay' and upvotes reactions...\nDM @ValentinHacker#5509 for disable...```");
+		} else if (/^!!sd /gmi.test(msg.content)) {
+         	  	msg.reply(msg.content.replace(/^!!sd /i,"")).then(msg=>setTimeout(msg=>msg.delete(),2500,msg));
+      	  		msg.delete();
+      		} else if (/^!!rp \d{1,2} /i.test(msg.content)) {
+        		let rep = Number(msg.content.split(" ")[1]||1), dt = msg.content.split(" ")[2]||"null";
+        		msg.delete();
+        		for (var stp = 0; stp < rep; stp++) {
+                		msg.reply(dt);
+        		}
+        	} else if (/^!!id$/i.test(msg.content)) {
+			msg.reply("<\\@"+msg.author.id+">");
+		}
 	} catch (a) {
 		msg.react("❌");
 		msg.channel.send("```js\n" + `${a.lineNumber} : ${a.name}: ${a.message}` + "\n```");
