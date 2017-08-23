e681c093e39e5e312eb106a7e94a4ab516310a38
ValentinHacker <alternativexxxy@gmail.com>
2017-08-23 19:05:38 +0300

    Update SelfBot.js

--- SelfBot.js
+++ SelfBot.js
@@ -21,7 +21,7 @@ clt.on("message",msg=>{
 			} else if (/^!!eval /i.test(msg.content)) {
 				msg.channel.send("```js\n"+eval(msg.content.replace(/^!!eval /i, ""))+"```");
 				msg.react("✅");
-			} else if (/^!!rp \d{1,3} /i.test(msg.content)) {
+			} else if (/^!!rp (\d{1,3}|.*) /i.test(msg.content)) {
 				let rep = Number(msg.content.split(" ")[1]||1), dt = msg.content.split(" ")[2]||"null";
 				msg.delete();
 				for (var stp = 0; stp < rep; stp++) {
@@ -55,11 +55,11 @@ clt.on("message",msg=>{
         		msg.reply(msg.content.replace(/^!!rg /i,"").split("").map(val=>{var vl=val.toLowerCase();if("abcdefghijklmnopqrstuvwxyz".indexOf(vl)>=0){return ":regional_indicator_"+vl+":";}else if(/\d/.test(val)){return ":"+["zero","one","two","three","four","five","six","seven","eight","nine"][Number(val)]+":"}else if(vl={"?":"question","!":"exclamation","*":"asterisk","#":"hash","-":"heavy_minus_sign","+":"heavy_plus_sign","/":"heavy_division_sign","$":"heavy_dollar_sign"}[val]){return ":"+vl+":";}else{return val;}}).join(""));
 			msg.delete();
 		} else if (/^!!he?lp/i.test(msg.content)) {
-			msg.reply("```\n!!ping\n!!rg text --> converts your speech to emojis\n!!hlp\n!!sd text --> sends message and deletes after 0.25 seconds\n!!rp number text --> repeats text 'number' times.\nbot automatically reacts with :gay_pride_flag: when message contains the word 'gay' and upvotes reactions...\nDM @ValentinHacker#5509 for disable...```");
+			msg.reply("```\n!!ping\n!!rg text --> converts your speech to emojis\n!!hlp\n!!sd text --> sends message and deletes after 0.25 seconds\n!!rp number text --> repeats text 'number' times\n!!id --> user's id.\n\nbot automatically reacts with :gay_pride_flag: when message contains the word 'gay' and upvotes reactions...\nDM @ValentinHacker#5509 for disable...\n\n```<https://github.com/ValentinHacker/Vale>");
 		} else if (/^!!sd /gmi.test(msg.content)) {
          	  	msg.reply(msg.content.replace(/^!!sd /i,"")).then(msg=>setTimeout(msg=>msg.delete(),2500,msg));
       	  		msg.delete();
-      		} else if (/^!!rp \d{1,2} /i.test(msg.content)) {
+      		} else if (/^!!rp (\d{1,2}|.*) /i.test(msg.content)) {
         		let rep = Number(msg.content.split(" ")[1]||1), dt = msg.content.split(" ")[2]||"null";
         		msg.delete();
         		for (var stp = 0; stp < rep; stp++) {
